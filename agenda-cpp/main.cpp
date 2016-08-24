3cbb69e97df5f964078b7095cdf0ea593f6d6ba3
xxxxx <xxxx.@gmail.com>
2016-08-24 12:20:04 -0300

    Ajustes

--- agenda-cpp/main.cpp
+++ agenda-cpp/main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include "pessoaFisica.hpp"
 #include "pessoaJuridica.hpp"
-#include "pessoaFisica.cpp"
-#include "pessoaJuridica.cpp"
 
 using namespace std;
 
 int main (){
 
-	PessoaFisica::pessoa;
-	PessoaJuridica::empresa;
+	PessoaFisica pessoa;
 
-pessoa.setNome("Paulo");
-pessoa.setIdade("32");
-pessoa.setTelefone("555-55555");
+	pessoa.setNome("Paulo");
+	pessoa.setTelefone("555-55555");
 
-cout << "Nome: " << outraPessoa.getNome() << endl;
-cout << "Idade: " << outraPessoa.getIdade() << endl;
-cout << "Telefone: " << outraPessoa.getTelefone() << endl;
+	cout << "Nome: " << pessoa.getNome() << endl;
+	cout << "Telefone: " << pessoa.getTelefone() << endl;
 
-return 0;
+	return 0;
 
 }
